9cfd8a44c80bb29649ab2452c455432c21eb8178
lolavalentinaperez <lolavalentinaperez981@gmail.com>
2025-05-22 13:52:48 -0300

    Primer commit
    Hice un comentario para probar

--- TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/TheCommonRoomApplication.java
+++ TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/TheCommonRoomApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TheCommonRoomApplication {
 
 	public static void main(String[] args) {
+		//Este es un comentario de prueba
 		SpringApplication.run(TheCommonRoomApplication.class, args);
 	}
 
