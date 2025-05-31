e2278af6063b997fb2f4bb8f5a66af5f96c82916
delfi-fenoy <delfina-noel-fenoy-rivas@estudiante.mdp.utn.edu.ar>
2025-05-31 17:01:25 -0300

    UserRequestDTO

---
+++ TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.thecommonroom.TheCommonRoom.controller;
+
+import com.thecommonroom.TheCommonRoom.dto.UserRequestDTO;
+import com.thecommonroom.TheCommonRoom.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    // Atributos
+
+    private final UserService userService;
+
+    // Metodos
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void register(@Valid @RequestBody UserRequestDTO userRequestDTO){
+//        userService.
+    }
+}
--- TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/repository/UserRepository.java
+++ TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.thecommonroom.TheCommonRoom.repository;
 
-public class UserRepository {
+import com.thecommonroom.TheCommonRoom.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
 }
