0fcf3c347f585455dcda9a1860c26e0a91b78639
lolavalentinaperez <lolavalentinaperez981@gmail.com>
2025-06-01 01:32:25 -0300

    Terminado de endpoint de iniciar sesión

--- TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/service/UserService.java
+++ TheCommonRoom/src/main/java/com/thecommonroom/TheCommonRoom/service/UserService.java
@@ -9,11 +9,14 @@ import com.thecommonroom.TheCommonRoom.model.User;
 import com.thecommonroom.TheCommonRoom.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -38,9 +41,17 @@ public class UserService implements UserDetailsService {
         userRepository.save(user); // Guardar en la base de datos
     }
 
+    ///Autentica al usuario al acer login
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-       ///Necesito el usuario
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+       User user= userRepository.findByUsername(username)
+               .orElseThrow(()-> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_"+ user.getRole()))
+        );
     }
 }
